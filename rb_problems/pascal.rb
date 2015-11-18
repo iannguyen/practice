57d07423a1b060f34159cd099d6bd286abd95edf
Ian Nguyen <iannknguyen@gmail.com>
2015-11-17 22:07:30 -0800

    more

---
+++ rb_problems/pascal.rb
@@ -0,0 +1,13 @@
+def factorial(n)
+  (1..n).inject(:*)
+end
+
+def binomial(n,k)
+  return 1 if n-k <= 0
+  return 1 if k <= 0
+  factorial(n) / ( factorial(k) * factorial( n - k ) )
+end
+
+def triangle(nth_line)
+  (0..nth_line).map { |e| binomial(nth_line, e) }
+end
---
+++ rb_problems/permutations.rb
@@ -0,0 +1,13 @@
+def permutations(arr)
+  return [arr] if arr.size == 1
+  first = arr.first
+  new_perms = []
+
+  perms = permutations(arr.drop(1))
+  perms.each do |perm|
+    (0..perm.size).each do |i|
+      new_perms << perm[0...i] + [first] + perm[i...perm.size]
+    end
+  end
+  new_perms
+end
