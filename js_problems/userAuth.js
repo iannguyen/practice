eeef983227ce186ffc8a97c7fd4003dc892a295d
Ian Nguyen <iannknguyen@gmail.com>
2015-11-19 17:55:05 -0800

    auth

---
+++ js_problems/userAuth.js
@@ -0,0 +1,28 @@
+(function(root) {
+  'use strict';
+
+  root.User = function User(userEmail) {
+    var email = userEmail;
+    var password;
+
+    this.getEmail = function() {
+      return email;
+    };
+
+    this.authenticate = function(userPassword) {
+      if (password === userPassword) {
+        return "success";
+      }
+    };
+
+    User.prototype.setPassword = function(userPassword) {
+      password = userPassword;
+    };
+
+    // this.setPassword = function(userPassword) {
+    //   password = userPassword;
+    // };
+
+  };
+
+}(this));
--- scratch.js
+++ scratch.js
@@ -1,19 +1,1 @@
 // scratch
-
-var User = function(email) {
-  this.email = email;
-};
-
-User.prototype.setPassword = function (password) {
-  Object.defineProperty(this, "password", {
-    set: function(password) {
-      this.password = password;
-    }
-  });
-};
-
-User.prototype.authenticate = function (password) {
-  if (this.password === password) {
-    return "success";
-  }
-};
