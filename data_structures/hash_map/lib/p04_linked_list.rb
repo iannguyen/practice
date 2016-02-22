5a4362cc57cfdda86afbff47ff2abf25965723c3
Ian Nguyen <iannknguyen@gmail.com>
2016-02-21 22:45:14 -0800

    complete linked_list

--- data_structures/hash_map/lib/p04_linked_list.rb
+++ data_structures/hash_map/lib/p04_linked_list.rb
@@ -1,8 +1,11 @@
 class Link
-  attr_accessor :key, :val, :next
+  attr_accessor :key, :val, :next, :prev
 
-  def initialize(key = nil, val = nil, nxt = nil)
-    @key, @val, @next = key, val, nxt
+  def initialize(key = nil, val = nil, nxt = nil, prv = nil)
+    @key = key
+    @val = val
+    @next = nxt
+    @prev = prv
   end
 
   def to_s
@@ -11,10 +14,14 @@ class Link
 end
 
 class LinkedList
-  attr_reader :head
+  include Enumerable
+  attr_reader :head, :tail
 
   def initialize
-    @head = Link.new
+    @head = Link.new(:head, :link)
+    @tail = Link.new(:tail, :link)
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -23,31 +30,54 @@ class LinkedList
   end
 
   def first
+    empty? ? nil : @head.next
   end
 
   def last
+    empty? ? nil : @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each { |link| return link.val if link.key == key }
   end
 
   def include?(key)
+    any? { |link| link.key == key }
   end
 
   def insert(key, val)
+    each { |link| return link.val = val if link.key == key }
+    new_link = Link.new(key, val, @tail, @tail.prev)
+    @tail.prev.next = new_link
+    @tail.prev = new_link
+    return "#{new_link.val} inserted."
   end
 
   def remove(key)
+    each do |link|
+      next unless link.key == key
+      link.prev.next = link.next
+      link.next.prev = link.prev
+      link.prev = nil
+      link.next = nil
+      return "#{link.val} removed."
+    end
   end
 
   def each
+    current_link = @head.next
+    until current_link == @tail
+      yield current_link
+      current_link = current_link.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
