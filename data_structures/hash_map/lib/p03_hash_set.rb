30bd676b4dcc9d3435a219a8fed3c1315cab5620
Ian Nguyen <iannknguyen@gmail.com>
2016-02-21 21:16:32 -0800

    complete hash_set

--- data_structures/hash_map/lib/p03_hash_set.rb
+++ data_structures/hash_map/lib/p03_hash_set.rb
@@ -4,23 +4,28 @@ class HashSet
   attr_reader :count
 
   def initialize(num_buckets = 8)
-    @store = Array.new(num_buckets) { Array.new }
+    @store = Array.new(num_buckets) { [] }
     @count = 0
   end
 
   def insert(key)
+    resize! if @count + 1 > num_buckets
+    self[key] << key
+    @count += 1
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    self[key].delete(key)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +33,9 @@ class HashSet
   end
 
   def resize!
+    prev = @store
+    @store = Array.new(num_buckets * 2) { [] }
+    @count = 0
+    prev.flatten.each { |el| insert(el) }
   end
 end
