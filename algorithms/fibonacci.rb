6c7fc57afb043a41c91958abf00901a7c01ce55d
Ian Nguyen <iannknguyen@gmail.com>
2016-02-28 00:01:06 -0800

    minor

--- algorithms/fibonacci.rb
+++ algorithms/fibonacci.rb
@@ -1,13 +1,14 @@
-# Function that returns the nth fibonacci number with O(n) time, O(1) memory
+# Function that returns the nth fibonacci number in O(n) time, O(1) memory
 
 def fibonacci(n)
-  return n if n < 2
+  return 0 if n.zero? || n == 1
+  return 1 if n == 2
 
   first = 0
   second = 1
   fib = nil
 
-  (n - 1).times do
+  (n - 2).times do
     fib = first + second
     first = second
     second = fib
