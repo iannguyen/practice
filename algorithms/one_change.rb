4c9ca8114f13a70703bdec943cc1fa9a5309dd81
Ian Nguyen <iannknguyen@gmail.com>
2016-02-29 22:17:24 -0800

    fix

--- algorithms/one_change.rb
+++ algorithms/one_change.rb
@@ -7,13 +7,20 @@ def one_change(str1, str2)
   diff = (str1.length - str2.length).abs
   return false if diff > 1
 
-  edits = 0
+  edits = diff
 
-  longer = str1.length >= str2.length ? str1 : str2
+  shorter = str1.length <= str2.length ? str1 : str2
 
-  longer.length.times do |i|
+  shorter.length.times do |i|
     edits += 1 unless str1[i] == str2[i]
+    return false if edits > 2
   end
 
-  edits.zero? || (edits == 1 && diff <= 1) || (edits == 2 && diff == 0)
+  edits <= 1 ? true : str1.split('').sort == str2.split('').sort
 end
+
+# Tests
+
+puts one_change('hello', 'holle')
+puts one_change('hello', 'htllb')
+puts one_change('hello', 'hellos')
