8ac080e0eb4f7b5ed9d1b8ce3837e8d7a181c705
Ian Nguyen <iannknguyen@gmail.com>
2016-03-01 19:21:19 -0800

    add problem

--- data_structures/trees_and_graphs/is_bst.rb
+++ data_structures/trees_and_graphs/is_bst.rb
@@ -2,6 +2,10 @@
 
 require_relative 'binary_search_tree'
 
-def bst?
+def bst?(node)
+  return true if node.nil? || node.left.nil? && node.right.nil?
+  return false if !node.left.nil? && node.left.value < node.value
+  return false if !node.right.nil? && node.right.value < node.value
 
+  bst?(node.right) && bst?(node.left)
 end
