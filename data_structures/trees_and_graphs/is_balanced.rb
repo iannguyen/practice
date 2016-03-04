6132ea81f958f9fc32dfd286efb6103575f1d773
Ian Nguyen <iannknguyen@gmail.com>
2016-03-03 19:02:11 -0800

    finish

--- data_structures/trees_and_graphs/is_balanced.rb
+++ data_structures/trees_and_graphs/is_balanced.rb
@@ -1,5 +1,19 @@
 # Write a function to determine if a Binary Search tree is balanced
 
-def balanced?
-  
+require_relative 'binary_search_tree'
+
+def balanced?(node)
+  return true if node.nil?
+
+  left_depth = depth(node.left)
+  right_depth = depth(node.right)
+  diff = (left_depth - right_depth).abs
+
+  balanced?(node.left) && balanced?(node.right) && diff <= 1
+end
+
+def depth(node)
+  return true if node.nil?
+
+  [depth(node.left), depth(node.right)].max + 1
 end
