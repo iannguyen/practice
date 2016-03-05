af2b8d8656b3f1ba4cb9ce8332c1a543b1948e48
Ian Nguyen <iannknguyen@gmail.com>
2016-03-04 20:13:16 -0800

    finish

---
+++ data_structures/linked_lists/kth_to_last.rb
@@ -0,0 +1,31 @@
+# Given a singly linked list and an integer k, return the kth to last node
+
+require_relative 'linked_list'
+
+class LinkedList
+  def kth_to_last(k)
+    fast = head
+    slow = head
+
+    k.times { fast = fast.next }
+
+    until fast.next.nil?
+      fast = fast.next
+      slow = slow.next
+    end
+
+    slow
+  end
+end
+
+# Tests
+
+ll = LinkedList.new
+ll.insert(1)
+ll.insert(2)
+ll.insert(3)
+ll.insert(4)
+ll.insert(5)
+ll.insert(6)
+
+puts ll.kth_to_last(2).value
--- data_structures/linked_lists/linked_list.rb
+++ data_structures/linked_lists/linked_list.rb
@@ -1,1 +1,24 @@
-# Create a Doubly Linked List class
+# Create a Singly Linked List class
+
+class Link
+  attr_accessor :value, :next
+
+  def initialize(v = nil, n = nil)
+    @value = v
+    @next = n
+  end
+end
+
+class LinkedList
+  attr_accessor :head
+
+  def initialize
+    @head = Link.new
+  end
+
+  def insert(v)
+    current = @head
+    current = current.next until current.next.nil?
+    current.next = Link.new(v)
+  end
+end
