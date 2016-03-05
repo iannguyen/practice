2c1c62c599d40a262957efc2c1624dede6209f34
Ian Nguyen <iannknguyen@gmail.com>
2016-03-04 20:39:00 -0800

    finish

---
+++ algorithms/maximal_subarray.rb
@@ -0,0 +1,23 @@
+# Given an array of positive and negative integers, return the largest sum of a continuous subarray
+
+def max_sub_sum(arr)
+  max_sum = nil
+  cur_sum = 0
+
+  arr.each_with_index do |num, idx|
+    if num < 0
+      max_sum = cur_sum if max_sum.nil? || cur_sum > max_sum
+      cur_sum = 0
+      next
+    end
+
+    cur_sum += num
+  end
+
+  max_sum
+end
+
+# Tests
+
+puts max_sub_sum([1,2,1,-1,5,-3,2])
+puts max_sub_sum([1,2,1,-1,5,10,-3,2])
