10b98791f0c14718b2a9576423d56a8ffa4c0fc7
Ian Nguyen <iannknguyen@gmail.com>
2016-03-05 23:11:01 -0800

    fix

--- algorithms/maximal_subarray.rb
+++ algorithms/maximal_subarray.rb
@@ -4,7 +4,7 @@ def max_sub_sum(arr)
   max_sum = nil
   cur_sum = 0
 
-  arr.each_with_index do |num, idx|
+  arr.each_with_index do |num, _idx|
     if num < 0
       max_sum = cur_sum if max_sum.nil? || cur_sum > max_sum
       cur_sum = 0
@@ -19,5 +19,5 @@ end
 
 # Tests
 
-puts max_sub_sum([1,2,1,-1,5,-3,2])
-puts max_sub_sum([1,2,1,-1,5,10,-3,2])
+puts max_sub_sum([1, 2, 1, -1, 5, -3, 2])
+puts max_sub_sum([1, 2, 1, -1, 5, 10, -3, 2])
