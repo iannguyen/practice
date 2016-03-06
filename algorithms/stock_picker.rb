b12eef889c62a96ea7d717247b0d41dea9632f0b
Ian Nguyen <iannknguyen@gmail.com>
2016-03-05 20:50:08 -0800

    add and finish

---
+++ algorithms/stock_picker.rb
@@ -0,0 +1,34 @@
+# Given an array of stock prices, find return the 2 days (indices) where the maximum profit is attainable
+# O(n) time, O(1) memory
+
+def stock_picker(arr)
+  queue = []
+  min = nil
+  max = nil
+  best_profit = nil
+
+  arr.each_with_index do |el, idx|
+    queue << el
+
+    min = { val: el, day: idx } if min.nil? || el < min[:val]
+    max = { val: el, day: idx } if max.nil? || el > max[:val]
+
+    cur_profit = max[:val] - min[:val] if min[:day] <= max[:day]
+
+    next unless best_profit.nil? ||
+                (cur_profit && cur_profit > best_profit[:profit])
+
+    best_profit = {
+      profit: cur_profit,
+      buy_day: min[:day],
+      sell_day: max[:day]
+    }
+  end
+
+  best_profit
+end
+
+# Tests
+
+puts stock_picker([10, 5, 20, 3, 6])
+puts stock_picker([1, 2, 20, 3, 50, 1])
