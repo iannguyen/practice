9601c668ec0f2ed7726b59a9027a1c15b6b4bb2d
Ian Nguyen <iannknguyen@gmail.com>
2016-03-10 23:53:53 -0800

    add and finish

---
+++ other/shuffle.rb
@@ -0,0 +1,12 @@
+# Write a function that shuffles an array
+
+def shuffle(arr)
+  new_arr = arr.dup
+
+  new_arr.each_index do |idx|
+    rand_idx = idx + rand(new_arr.count - idx)
+    new_arr[idx], new_arr[rand_idx] = new_arr[rand_idx], new_arr[idx]
+  end
+
+  new_arr
+end
