3a451ad76a02a114450ebae6315a917ee8fd86d6
Ian Nguyen <iannknguyen@gmail.com>
2016-03-11 20:57:11 -0800

    finish

--- data_structures/trees_and_graphs/breadth-first-search.rb
+++ data_structures/trees_and_graphs/breadth-first-search.rb
@@ -1,1 +1,11 @@
 # Write a breadth-first-searc function
+
+def bfs(node)
+  queue = [node]
+
+  until queue.empty?
+    current = queue.shift
+    puts "Passing through #{current.value}"
+    queue.concat(current.children) if current.children
+  end
+end
--- data_structures/trees_and_graphs/depth_first_search.rb
+++ data_structures/trees_and_graphs/depth_first_search.rb
@@ -1,1 +1,6 @@
 # Write a depth-first-search function
+
+def dfs(node)
+  puts "Passing through #{node.value}"
+  node.children.each { |child| dfs(child) }
+end
