4f9042daf8313afac40955d40fcde6e88ce73546
Ian Nguyen <iannknguyen@gmail.com>
2016-03-16 00:59:53 -0700

    fix

--- algorithms/base.rb
+++ algorithms/base.rb
@@ -1,36 +1,33 @@
 # given a number, convert it to excel column names
 # 1 - > A, 25 -> Y, 27 -> AA 676-> ZZ 677 AAA
 
-(100.to_s(2))
-
 def base_two(num)
-    result = ""
+  result = ''
 
-    until num.zero?
-        rem = num % 2
-        num = num / 2
+  until num.zero?
+    rem = num % 2
+    num /= 2
+    result += rem.to_s
+  end
 
-        result += rem.to_s
-    end
-
-    result.reverse
+  result.reverse
 end
-
 
 def base_excel(num)
-    result = ""
-    nums = (0..25).to_a
-    letters = ("A".."Z").to_a
-    base_map = Hash[nums.zip(letters)]
+  result = ''
+  nums = (0..25).to_a
+  letters = ('A'..'Z').to_a
+  base_map = Hash[nums.zip(letters)]
 
-    until num.zero?
-        rem = (num - 1) % 26
-        num = (num - 1) / 26
+  until num.zero?
+    rem = (num - 1) % 26
+    num = (num - 1) / 26
+    result += base_map[rem]
+  end
 
-        result += base_map[rem]
-    end
-
-    result.reverse
+  result.reverse
 end
+
+# Tests
 
 print base_excel(26)
