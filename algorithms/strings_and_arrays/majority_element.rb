1e52b95e1b1e5b2a6c38af07854a18827feb9e8d
Ian Nguyen <iannknguyen@gmail.com>
2016-03-28 23:21:36 -0700

    add and finish

--- algorithms/strings_and_arrays/majority_element.rb
+++ algorithms/strings_and_arrays/majority_element.rb
@@ -2,5 +2,21 @@
 #
 # You may assume that the array is non-empty and the majority element always exist in the array.
 
+# Bayer-Moore voting algorithm
 def majority_element(nums)
+  major = nums.first
+  count = 0
+
+  nums.each do |num|
+    if major == num
+      count += 1
+    else
+      count.zero? ? major = num : count -= 1
+    end
+  end
+  major
 end
+
+# Tests
+
+p majority_element([1, 2, 1, 3, 1])
