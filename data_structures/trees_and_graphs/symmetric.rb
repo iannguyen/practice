c9c5f383b46ffa1fb61b81a4f148d5db874ca0f4
Ian Nguyen <iannknguyen@gmail.com>
2016-04-02 17:41:11 -0700

    add and finish

---
+++ data_structures/trees_and_graphs/symmetric.rb
@@ -0,0 +1,12 @@
+# Write a function to determine if a binary tree is symmetric
+
+def is_symmetric(root)
+  return true unless root
+  symmetric(root.left, root.right)
+end
+
+def symmetric(left, right)
+  return left == right if left.nil? || right.nil?
+  return false unless left.val == right.val
+  symmetric(left.right, right.left) && symmetric(left.left, right.right)
+end
