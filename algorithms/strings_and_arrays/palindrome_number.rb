1d8abeddbe2d552fb046f25c79880c6f110b8dab
Ian Nguyen <iannknguyen@gmail.com>
2016-04-06 23:36:56 -0700

    add and finish

--- algorithms/strings_and_arrays/palindrome_number.rb
+++ algorithms/strings_and_arrays/palindrome_number.rb
@@ -1,9 +1,17 @@
 # Given a number, determine whether the number is a palindrome. Do this without extra space.
 
 def palindrome(num)
-
+  return false if num < 0
+  return true if num < 10
+  left = 0
+  until left >= num
+    left = left * 10 + num % 10
+    num /= 10
+  end
+  left == num || left / 10 == num
 end
 # Tests
 
+p palindrome(123)
 p palindrome(10)
 p palindrome(1001)
