0126ad1d14294c0072039bbe8310894a1d6905ae
Ian Nguyen <iannknguyen@gmail.com>
2016-04-09 16:21:07 -0700

    add and finish

--- data_structures/linked_lists/reverse_linked_list.rb
+++ data_structures/linked_lists/reverse_linked_list.rb
@@ -1,7 +1,38 @@
-# Reverse a Linked List
+# Reverse a singly Linked List
 
-require_relative 'linked_list'
+class Node
+  attr_accessor :next, :val
+  def initialize(val)
+    @val = val
+    @next = nil
+  end
+end
 
 def reverse_ll(head)
+  return head unless head
+  prv = nil
+  cur = head
+  nxt = cur.next
 
+  while cur && cur.next
+    cur.next = prv
+    prv = cur
+    cur = nxt
+    nxt = cur.next
+  end
+
+  cur.next = prv
+  cur
 end
+
+# Tests
+
+a = Node.new(1)
+cur = a
+
+(2..5).each do |num|
+  cur.next = Node.new(num)
+  cur = cur.next
+end
+
+p reverse_ll(a)
